865d69dd0dbbe147bf14de0b740749a64ae72b7d
Danna <dannamillan27@outlook.es>
2024-10-24 15:01:34 -0500

    Se agregó la base de datos

--- clases/Conexion.php
+++ clases/Conexion.php
@@ -2,9 +2,9 @@
     class Conexion {
 
         // "Credenciales" de conexión
-        public $servidor = 'localhost';
-        public $usuario = 'root';
-        public $password = '';
+        public $servidor = 'unieventos.chyqgm60qwri.us-east-1.rds.amazonaws.com';
+        public $usuario = 'admin';
+        public $password = 'jodahezo27?';
         public $database = 'unieventos';
         public $port = 3306;
         // 
